fd14629fbc42f0086e39de70044ca010b42776e2
Yaniv Ironi <iryaniv@gmail.com>
2018-04-04 20:53:46 +0300

    Added table

--- src/components/ResultsTable.jsx
+++ src/components/ResultsTable.jsx
@@ -1,18 +1,20 @@
 import './ResultsTable.css';
 import React from 'react';
+import BootstrapTable from 'react-bootstrap-table-next';
 
 module.exports = class ResultsTable extends React.Component {
     render() {
+        const columns = [{
+            dataField: '_time',
+            text: 'Time'
+        }, {
+            dataField: '_raw',
+            text: 'Raw'
+        }
+        ];
+
         return (
-            <table className="resultstable">
-                <tbody>
-                    {this.props.results.map((row, i) =>
-                        <tr className="clickable" onClick={this.props.showSelected} key={i}>
-                            {row["_time"] + " - " + row["_raw"]}
-                            </tr>
-                    )}
-                </tbody>
-            </table>
+            <BootstrapTable keyField='id' data={ this.props.results } columns={ columns } />
         );
     }
-}
+};
