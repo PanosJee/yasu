155140cf1cc135f0f132ac7baf957f3d427bc820
Omer Levi <omerlevi13@gmail.com>
2018-04-07 20:14:00 +0300

    Added Loader component

---
+++ src/components/Loader.jsx
@@ -0,0 +1,12 @@
+import './Loader.css';
+import React from 'react';
+
+class Loader extends React.Component {
+    render() {
+        return (
+            <div className="loader"></div>
+        );
+    }
+}
+
+export default Loader;
