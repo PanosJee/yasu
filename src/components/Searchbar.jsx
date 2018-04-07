f3303db2d8911002045c27fd3fb42a8178b837c2
Yaniv Ironi <iryaniv@gmail.com>
2018-04-07 18:46:54 +0300

    Now actuallty removed all react-strap deps.

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,13 +1,13 @@
 import './Searchbar.css'
 import React from 'react';
-import { Button, Input, Form } from 'reactstrap';
+import { Button, Input, Form, FormControl } from 'react-bootstrap';
 
 class Searchbar extends React.Component {
     render() {
         return (
             <div className="searchbar-form">
                 <Form inline>
-                    <Input type="textarea" id="searchbar-searchbox" placeholder="Search.." onChange={this.props.updateSearchType} />
+                    <FormControl type="textarea" id="searchbar-searchbox" placeholder="Search.." onChange={this.props.updateSearchType} />
                     <Button size="lg" className="btn searchbar-submit" onClick={this.props.search} >{this.props.searchType}</Button>
                 </Form>
             </div>
